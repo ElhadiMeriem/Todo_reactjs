82bcc812297bf562b19c98c13b4afbb17425e9bc
ElhadiMeriem <meriem.elhadi@sbsdatafactory.com>
2022-11-29 16:06:56 +0100

    third commit reactjs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
-import {Route ,Routes , Link , BrowserRouter as Router} from 'react-router-dom';
+import {Route ,Routes  , BrowserRouter as Router} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css' ;
 import Todo from './Todo';
  
 
 function App() {
   return (
-    <Router>
-      <nav className='navbar navbar-expand navbar-band m-5'>
-        <ul className='navbar-nav'>
-          <li>
-            <Link className="nav-link" to="/todo">Todo</Link>
-          </li>    
-        </ul>
-      </nav>  
+    <Router> 
       <div className='container'>
         <Routes>
-          <Route  path="/todo" element={<Todo/>} ></Route> 
+          <Route  path="/" element={<Todo/>} ></Route> 
         </Routes>
       </div>
     </Router>
--- src/Todo.js
+++ src/Todo.js
@@ -1,92 +1,103 @@
+
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css" ;
 
-class Todo extends Component {
+class Test extends Component {
 
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            taskvalue : "",
-            tasks : [
-                {id : 1 , task :"Buy Groceries for Next week"} ,
-                {id : 2 , task : "Renew Car Insurrance" },
-                {id : 3 , task: "Sign up for online course"}
-            ]
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskvalue: "",
+      tasks: [
+        { id: 1, task: "Buy Groceries for Next week" },
+        { id: 2, task: "Renew Car Insurrance" },
+        { id: 3, task: "Sign up for online course" }
+      ]
     }
-    add = (event) =>{
-        event.preventDefault();
-        let newObject = {
-            id :  [...this.state.tasks].pop().id+1 ,
-            task : this.state.taskvalue,
-        }
-        this.setState(
-            {
-                tasks : [...this.state.tasks , newObject]
-
-            }
-        )
-        
-
+  }
+  add = (event) => {
+    event.preventDefault();
+    let newObject = {
+      id: [...this.state.tasks].pop().id + 1,
+      task: this.state.taskvalue,
     }
-    
-    handleChange=(e) =>
-    {
-        let task = e.target.value;
-        this.setState(
-            {
-            taskvalue : task
-            }
-        )
+    this.setState(
+      {
+        tasks: [...this.state.tasks, newObject]
 
-    }
-    delete = (t) =>{
-        let copiedTasks = [...this.state.tasks];
-        let index = copiedTasks.findIndex(x=>x.id === t.id); 
-        copiedTasks.splice(index, 1);
-        this.setState(
-            {
-                tasks : copiedTasks
-            }
-        )
-    }
-    render() {
+      }
+    )
 
-        return (
-            <div>
-                <div>
-                    <h1>{this.state.message}</h1>
-                    <form onSubmit={this.add}>
-                        <div className ="form-group">
-                            <label htmlFor="exampleFormControlInput1">Add New Skill</label>
-                            <input type="text" className="form-control"  onChange={this.handleChange} value={this.state.taskvalue} placeholder="" />
-                            <button type="submit" className="btn btn-primary">Submit</button>
+
+  }
+
+  handleChange = (e) => {
+    let task = e.target.value;
+    this.setState(
+      {
+        taskvalue: task
+      }
+    )
+
+  }
+  delete = (t) => {
+    let copiedTasks = [...this.state.tasks];
+    let index = copiedTasks.findIndex(x => x.id === t.id);
+    copiedTasks.splice(index, 1);
+    this.setState(
+      {
+        tasks: copiedTasks
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <section className="vh-100">
+          <div className="container py-5 h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col col-lg-9 col-xl-7">
+                <div className="card rounded-3">
+                  <div className="card-body p-4">
+                    <h4 className="text-center my-3 pb-3">To Do App</h4>
+                    <form className="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2" onSubmit={this.add}>
+                      <div className="col-12">
+                        <div className="form-outline">
+                          <input type="text"  className="form-control" onChange={this.handleChange} value={this.state.taskvalue}/>
                         </div>
+                      </div>
+                      <div className="col-12">
+                        <button type="submit" className="btn btn-primary">Save</button>
+                      </div>
                     </form>
-                </div>
-                <div>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                            <th scope="col">id</th>
-                            <th scope="col">Tasks</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.tasks.map((t,index)=>
+                    <table className="table mb-4">
+                      <thead>
+                        <tr>
+                          <th scope="col">No.</th>
+                          <th scope="col">Todo item</th>
+                          <th scope="col">Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      {this.state.tasks.map((t,index)=>
                                 <tr key={t.id}>
-                                <td>{t.id}</td>
+                                <td >{t.id}</td>
                                 <td>{t.task}</td>
                                 <td><button type="button" className="btn btn-danger" onClick={()=>this.delete(t)}> Delete</button></td>
                                 </tr>
                             )}
-                        </tbody>
-                    </table> 
-                </div>                
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
-export default Todo;
+export default Test;
